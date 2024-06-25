dfe33848046e1c7da085b79eaa57707c331efcb9
CaioCLDias <caio.lorenzon.dias@gmail.com>
2024-06-25 20:31:52 -0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/NOME-DO-REPOSITORIO/'
-      : '/'
+  transpileDependencies: true,
+  publicPath: '/'
 
   
 })
