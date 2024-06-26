225550562fb2a4b06fe036b17544b08d5ac48d2d
CaioCLDias <caio.lorenzon.dias@gmail.com>
2024-06-25 21:21:08 -0300

    corrigindo bugs

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import AboutPage from '../views/AboutPage.vue';
 import ProjectsPage from '../views/ProjectsPage.vue';
 
 const routes = [
-  { path: '/', name: 'HomePage', component: HomePage },
-  { path: '/about', name: 'AboutPage', component: AboutPage },
-  { path: '/projects', name: 'ProjectsPage', component: ProjectsPage },
+  { path: '/', component: HomePage },
+  { path: '/about', component: AboutPage },
+  { path: '/projects', component: ProjectsPage }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
