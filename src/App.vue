5c4b4b569108d93dbe830c530fb2e7559a5f8dcc
CaioCLDias <caio.lorenzon.dias@gmail.com>
2024-06-25 21:11:00 -0300

    alterações

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
           <img src="./assets/icons/github.svg" alt="GitHub" class="w-6 h-6">
         </a>
       </div>
-      <p>&copy; 2024 Caio Dias. Todos os direitos reservados.</p>
+      <p>&copy; 2024 Caio Dias</p>
     </footer>
   </div>
 </template>
